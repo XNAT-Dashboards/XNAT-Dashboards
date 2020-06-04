dcfa974f00477da12c54d3f040e53d1d4b68701a
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-04 16:30:42 +0530

    Add for test_data_fetcher
    Test added for subject, project, experiments and scans with test if no network

--- tests/test_data_fetcher.py
+++ tests/test_data_fetcher.py
@@ -1,26 +1,67 @@
 from pyxnat_connection import data_fetcher
+import pytest
 
-fetch_object = data_fetcher.Fetcher(name='testUser',
-                                    password='testPassword',
+fetch_object_connected = data_fetcher.Fetcher(name='testUser',
+                                            password='testPassword',
                                     instance_url='https://central.xnat.org')
 
 
-def test_get_project_details():
+fetch_object_disconnected = data_fetcher.Fetcher(name='testUser',
+                                                 password='testPassword',
+                                    instance_url='https://central.xnat.org')
 
-    assert type(fetch_object.get_projects_details())\
-           == dict or fetch_object.get_projects_details()\
-           == 1, "Correct"
+
+def test_get_projects_details():
+
+    project_details = fetch_object_connected.get_projects_details()
+
+    assert type(project_details['number_of_projects']) == int
+    assert type(project_details['project_mr_ct_pet']) == dict
+
+
+def test_get_subjects_details():
+
+    subject_details = fetch_object_connected.get_subjects_details()
+
+    assert type(subject_details['number_of_subjects']) == int
+    assert type(subject_details['age']) == list
+    assert type(subject_details['gender']) == list
+    assert type(subject_details['handedness']) == list
+    assert type(subject_details['subjects_per_project']) == dict
 
 
 def test_get_experiments_details():
 
-    assert type(fetch_object.get_experiments_details())\
-           == dict or fetch_object.get_experiments_details()\
-           == 1, "Correct"
+    experiment_details = fetch_object_connected.get_experiments_details()
+
+    assert type(experiment_details['number_of_experiments']) == int
+    assert type(experiment_details['experiments_per_subject']) == dict
+    assert type(experiment_details['experiments_per_project']) == dict
+    assert type(experiment_details['experiment_types']) == dict
 
 
-def test_get_scan_details():
+def test_get_scans_details():
 
-    assert type(fetch_object.get_scans_details())\
-           == dict or fetch_object.get_scans_details()\
-           == 1, "Correct"
+    experiment_details = fetch_object_connected.get_scans_details()
+
+    assert type(experiment_details['number_of_scans']) == int
+    assert type(experiment_details['scans_per_subject']) == dict
+    assert type(experiment_details['scans_per_project']) == dict
+    assert type(experiment_details['scans_per_experiment']) == dict
+    assert type(experiment_details['usable_scans']) == int
+    assert type(experiment_details['unusable_scans']) == int
+    assert type(experiment_details['scan_types']) == dict
+    assert type(experiment_details['xsi_scan_types']) == dict
+
+
+@pytest.mark.disable_socket
+def test_get_details_disconnected():
+
+    # Network access removed using pytest_socket
+    assert fetch_object_disconnected.get_projects_details() == 1
+
+    assert fetch_object_disconnected.get_subjects_details() == 1
+
+    assert fetch_object_disconnected.get_experiments_details() == 1
+
+    assert fetch_object_disconnected.get_scans_details() == 1
