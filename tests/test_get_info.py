14574d4d75ff21397a898908ee8c9bc1ded648ab
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-05 22:09:31 +0530

    Test for get_info()
    Return type and number of keys present in dict checking

---
+++ tests/test_get_info.py
@@ -0,0 +1,13 @@
+from pyxnat_connection import get_info
+
+info_object = get_info.GetInfo(user='testUser',
+                               password='testPassword',
+                               server='https://central.xnat.org')
+
+
+def test_info():
+
+    info = info_object.get_info()
+
+    assert type(info) == dict   # Return type should be a dict
+    assert len(info) == 15       # Currently x dicts to be returned
