3be1af832795445de6f4f1d1c831dec8f7a48f99
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-07 12:53:11 +0530

    Fix if exception raised by data_fetcher
    If exception is raised in data_fetcher, it will return 1 and no further processing will be done on that method call

--- pyxnat_connection/get_info.py
+++ pyxnat_connection/get_info.py
@@ -30,35 +30,41 @@ class GetInfo:
         '''
 
         stats = {}
-        # preprocessing required in project data for number of projects
+        final_json_dict = {}
+
+        # Preprocessing required in project data for number of projects
         projects_details = self.fetcher_object.get_projects_details()
-        stats['Projects'] = projects_details['number_of_projects']
-        del projects_details['number_of_projects']
+        # If some error in connection 1 will be returned and we will
+        # not go further
+        if(projects_details != 1):
+            stats['Projects'] = projects_details['number_of_projects']
+            del projects_details['number_of_projects']
+            final_json_dict.update(projects_details)
 
         # Pre processing for subject details required
         subjects_details = self.fetcher_object.get_subjects_details()
-        stats['Subjects'] = subjects_details['number_of_subjects']
-        del subjects_details['number_of_subjects']
+        if(subjects_details != 1):
+            stats['Subjects'] = subjects_details['number_of_subjects']
+            del subjects_details['number_of_subjects']
+            final_json_dict.update(subjects_details)
 
-        # No need in pre processing experiment details
+        # Pre processing experiment details
         experiments_details = self.fetcher_object.get_experiments_details()
-        stats['Experiments'] = experiments_details['number_of_experiments']
-        del experiments_details['number_of_experiments']
+        if(experiments_details != 1):
+            stats['Experiments'] = experiments_details['number_of_experiments']
+            del experiments_details['number_of_experiments']
+            final_json_dict.update(experiments_details)
 
-        # No need in pre processing scans details
+        # Pre processing scans details
         scans_details = self.fetcher_object.get_scans_details()
-        stats['Scans'] = scans_details['number_of_scans']
-        del scans_details['number_of_scans']
+        if(scans_details != 1):
+            stats['Scans'] = scans_details['number_of_scans']
+            del scans_details['number_of_scans']
+            final_json_dict.update(scans_details)
 
         stat_final = {'Stats': stats}
 
-        final_json_dict = {}
-
         final_json_dict.update(stat_final)
-        final_json_dict.update(projects_details)
-        final_json_dict.update(subjects_details)
-        final_json_dict.update(experiments_details)
-        final_json_dict.update(scans_details)
 
         # Final dictionary which will act as a json file
         return final_json_dict
