fa91e89bb8786a9ec688119befd277ab1bee82d2
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-17 18:56:25 +0530

    Plotly update
    Add the chart generator, currently only support bar chart

---
+++ app/static/dashboards/js/plotly_chart_generator.js
@@ -0,0 +1,43 @@
+
+function chart_generator(json){
+    var graph_name = "";
+    var graph_info = {};
+    for (var g_name in json){
+        graph_name = g_name;
+        alert(g_name);
+        graph_info = json[g_name];
+    }
+    barchart_generator(graph_name,graph_info)
+}  
+
+function barchart_generator(graph_name, graph_info){
+    alert(graph_name);
+    x_axis = [];
+    y_axis = [];
+    for (x in graph_info){
+        if(x == 'graph_type' || x == 'id'){
+            continue;
+        }else{
+            x_axis.push(x);
+            y_axis.push(graph_info[x]);
+        }
+    }
+
+    var data = [
+        {
+          x: x_axis,
+          y: y_axis,
+          type: 'bar'
+        }
+      ];
+
+      var layout = {
+        title: graph_name,
+        showlegend: false
+    };
+
+    var config = {responsive: true}
+    
+    Plotly.newPlot('graph_body'+graph_info['id'], data, layout, config);
+
+}
