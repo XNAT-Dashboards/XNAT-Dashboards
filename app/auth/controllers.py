778b1e1311e969c451ab5263111291cb33dbee20
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-17 19:36:03 +0530

    add url_for for dynamic change in url

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -1,5 +1,6 @@
 # Import flask dependencies
-from flask import Blueprint, request, redirect, render_template, session
+from flask import Blueprint, request, redirect,\
+                  render_template, session, url_for
 
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 auth = Blueprint('auth', __name__, url_prefix='/auth')
@@ -13,7 +14,7 @@ def stats():
 
         user_detail = request.form
         session['user_d'] = user_detail
-        return redirect("dashboards.stats")
+        return redirect(url_for("dashboards.stats"))
 
     else:
 
