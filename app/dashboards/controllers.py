123fc0dec372734829444feaface21b64b893672
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-19 19:43:43 +0530

    add routes for post request

--- app/dashboards/controllers.py
+++ app/dashboards/controllers.py
@@ -1,22 +1,27 @@
 # Import flask dependencies
-from flask import Blueprint, session, render_template
+from flask import Blueprint, render_template, request
 from pyxnat_connection import graph_generator
 
 # Define the blueprint: 'dashboards', set its url prefix: app.url/dashboards
 dashboards = Blueprint('dashboards', __name__, url_prefix='/dashboards')
 
 
+data = {}
+
+
 # Set the route and accepted methods
-@dashboards.route('/stats/', methods=['GET'])
+@dashboards.route('/stats/', methods=['POST', 'GET'])
 def stats():
 
-    user_details = session['user_d']
-    username = user_details['username']
-    password = user_details['password']
-    server = user_details['server']
-
-    data = graph_generator.GraphGenerator(username,
-                                          password,
-                                          server).graph_generator()
-
-    return render_template('dashboards/stats_dashboards.html', data=data)
+    if(request.method == "POST"):
+        user_details = request.form
+        username = user_details['username']
+        password = user_details['password']
+        server = user_details['server']
+        global data
+        data = graph_generator.GraphGenerator(username,
+                                            password,
+                                            server).graph_generator()
+        return 'correct'
+    else:
+        return render_template('dashboards/stats_dashboards.html', data=data)
