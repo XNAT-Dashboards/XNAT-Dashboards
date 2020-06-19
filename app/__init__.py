ab4187fa2c4529c561ec0470e1c9edd15655fc9c
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-19 19:41:09 +0530

    added route for redirection

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 # Import flask and template operators
-from flask import Flask
+from flask import Flask, redirect
 from app.dashboards.controllers import dashboards
 from app.auth.controllers import auth
 
 # Define the WSGI application object
 app = Flask(__name__)
+
+
+# Set the redirecting route for dashboard
+@app.route('/')
+def stats():
+    return redirect('auth/login')
+
 
 # Configurations
 app.config.from_object('config')
