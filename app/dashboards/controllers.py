21570fba7a49bffb536b97ab1e4710626ed37ee8
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-29 20:23:21 +0530

    project list additon for owned, collab and member projects

--- app/dashboards/controllers.py
+++ app/dashboards/controllers.py
@@ -1,12 +1,13 @@
 # Import flask dependencies
 from flask import Blueprint, render_template, session, request, redirect, url_for
-from generators import graph_generator, project_generator
+from generators import graph_generator
 
 # Define the blueprint: 'dashboards', set its url prefix: app.url/dashboards
 dashboards = Blueprint('dashboards', __name__, url_prefix='/dashboards')
 
 graph_data = {}
 project_list = []
+project_list_ow_co_me = []
 username = ''
 server = ''
 
@@ -23,13 +24,16 @@ def stats():
         server = user_details['server']
         global graph_data
         global project_list
-        graph_data = graph_generator.GraphGenerator(username,
-                                                    password,
-                                                    server).graph_generator()
-        project_list = project_generator.projectGenerator(
-                                                        username,
-                                                        password,
-                                                        server).project_list()
+        global project_list_ow_co_me
+        plotting_object = graph_generator.GraphGenerator(username,
+                                                         password,
+                                                         server)
+        graph_data = plotting_object.graph_generator()
+        project_lists = plotting_object.project_list_generator()
+        print(project_list)
+        project_list = project_lists[0]
+        project_list_ow_co_me = project_lists[1]
+        print(project_lists[1])
 
         return 'correct'
     else:
@@ -37,9 +41,11 @@ def stats():
             session['error'] = graph_data
             return redirect(url_for('auth.login'))
         else:
+            print(project_list_ow_co_me)
             return render_template('dashboards/stats_dashboards.html',
                                    graph_data=graph_data,
                                    project_list=project_list,
+                                   project_list_ow_co_me=project_list_ow_co_me,
                                    username=username.capitalize(),
                                    server=server)
 
