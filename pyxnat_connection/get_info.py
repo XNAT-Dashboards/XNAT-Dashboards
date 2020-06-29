2405401f8de89610b22c46d7d370aede391fef57
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-06-29 20:22:23 +0530

    Code refactoring

--- pyxnat_connection/get_info.py
+++ pyxnat_connection/get_info.py
@@ -36,7 +36,7 @@ class GetInfo:
         projects_details = self.fetcher_object.get_projects_details()
         # If some error in connection 1 will be returned and we will
         # not go further
-        if(type(projects_details) != int):
+        if type(projects_details) != int:
             stats['Projects'] = projects_details['Number of Projects']
             del projects_details['Number of Projects']
         else:
@@ -44,19 +44,19 @@ class GetInfo:
 
         # Pre processing for subject details required
         subjects_details = self.fetcher_object.get_subjects_details()
-        if(subjects_details != 1):
+        if subjects_details != 1:
             stats['Subjects'] = subjects_details['Number of Subjects']
             del subjects_details['Number of Subjects']
 
         # Pre processing experiment details
         experiments_details = self.fetcher_object.get_experiments_details()
-        if(experiments_details != 1):
+        if experiments_details != 1:
             stats['Experiments'] = experiments_details['Number of Experiments']
             del experiments_details['Number of Experiments']
 
         # Pre processing scans details
         scans_details = self.fetcher_object.get_scans_details()
-        if(scans_details != 1):
+        if scans_details != 1:
             stats['Scans'] = scans_details['Number of Scans']
             del scans_details['Number of Scans']
 
@@ -70,10 +70,10 @@ class GetInfo:
 
         return final_json_dict
 
-    def get_info(self):
-
-        return self.__preprocessor()
-
     def get_project_list(self):
 
         return self.fetcher_object.get_projects_details_specific()
+
+    def get_info(self):
+
+        return self.__preprocessor()
