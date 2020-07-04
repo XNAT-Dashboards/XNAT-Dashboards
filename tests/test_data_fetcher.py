a5c7c97774ff902900a16984fef926595fc87bf4
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-07-04 13:26:40 +0530

    Add specific exception handling

--- tests/test_data_fetcher.py
+++ tests/test_data_fetcher.py
@@ -81,21 +81,3 @@ def test_get_projects_details_specific():
            == dict
     assert fetch_object_disconn_pwd_incorrect.get_projects_details_specific() == 1
     assert fetch_object_disconn_url_incorrect.get_projects_details_specific() == 1
-
-
-@pytest.mark.disable_socket
-def test_get_details_disconnected():
-
-    fetch_object_disconnected = data_fetcher.Fetcher(name='testUser',
-                                                     password='testPassword',
-                                                     server='https://central.xnat.org',
-                                                     ssl=False)
-
-    # Network access removed using pytest_socket
-    assert fetch_object_disconnected.get_projects_details() == 1
-
-    assert fetch_object_disconnected.get_subjects_details() == 1
-
-    assert fetch_object_disconnected.get_experiments_details() == 1
-
-    assert fetch_object_disconnected.get_scans_details() == 1
