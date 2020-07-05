543e09e2ce1bd9af2294d46995877b405b1a3262
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-07-05 15:26:22 +0530

    config update

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ from app.init_database import mongo
 
 # Define the WSGI application object
 app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
-app.config['MONGO_URI'] = 'mongodb+srv://testUser:testPassword@cluster0.x38yt.gcp.mongodb.net/xnat_dashboards?retryWrites=true&w=majority'
-app.config['MONGO_DB'] = 'xnat_dashboards'
+# Configurations
+app.config.from_object('config')
+
 mongo.init_app(app)
 
 
@@ -18,9 +18,6 @@ mongo.init_app(app)
 def stats():
     return redirect('auth/login')
 
-
-# Configurations
-app.config.from_object('config')
 
 app.register_blueprint(dashboards)
 app.register_blueprint(auth)
