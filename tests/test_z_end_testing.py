a341e49fc3bb9110e9cb55287d3cf136932c36ea
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-07-07 11:13:19 +0530

    rename testing file for running as last test

---
+++ tests/test_z_end_testing.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import sys
+import json
+from os.path import dirname, abspath
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+
+def test_end_testing():
+
+    with open('utils/db_config.json') as json_file:
+        db_json = json.load(json_file)
+
+    client = MongoClient(db_json['url'])
+    db = client['xnat_dashboards']
+
+    db.users.remove({'username': 'testUser'})
+    db.users_data.remove({'username': 'testUser'})
+
+    client = MongoClient(db_json['test_url'])
+    db2 = client['xnat_dashboards']
+
+    db2.users.remove({'username': 'testUser'})
+    db2.users_data.remove({'username': 'testUser'})
+
+    assert True
