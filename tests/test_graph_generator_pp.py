619c83692446eff5da7decf1a250f966a631ae55
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-07-10 20:32:24 +0530

    Test for graph generator
    DB view, DB with per project view and API per project view

---
+++ tests/test_graph_generator_pp.py
@@ -0,0 +1,20 @@
+from generators import graph_generator_pp
+from utils import input_processor
+import pathlib
+
+graph_object = graph_generator_pp.GraphGenerator(
+    "testUser",
+    "testPassword",
+    "https://central.xnat.org",
+    ssl=False,
+    project_id='CENTRAL_OASIS_CS')
+
+
+def test_graph_preprocessor():
+
+    assert type(graph_object.graph_pre_processor()) == dict
+
+
+def test_graph_generator():
+
+    assert type(graph_object.graph_generator()) == list
