76a666d1e8f400646b6724f710428d62e055b6d1
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-07-15 20:26:19 +0530

    Add file opening code to model.py

---
+++ app/dashboards/model.py
@@ -0,0 +1,49 @@
+from app.init_database import mongo
+import pickle
+
+
+def load_user_pk(username):
+
+    try:
+        with open('pickles/users/'+username+'.pickle', 'rb') as handle:
+            user = pickle.load(handle)
+    except FileNotFoundError:
+        return None
+
+    return user
+
+
+def load_users_data_pk(username):
+
+    try:
+        with open('pickles/users_data/'+username+'.pickle', 'rb') as handle:
+            user_data = pickle.load(handle)
+
+    except FileNotFoundError:
+        return None
+
+    return user_data
+
+
+def load_resources_pk(username):
+
+    try:
+        with open('pickles/resources/'+username+'.pickle', 'rb') as handle:
+            resources = pickle.load(handle)
+
+    except FileNotFoundError:
+        return None
+
+    return resources
+
+
+def load_resources_bbrc_pk(username):
+
+    try:
+        with open('pickles/resources/'+username+'bbrc.pickle', 'rb') as handle:
+            resources_bbrc = pickle.load(handle)
+
+    except FileNotFoundError:
+        return None
+
+    return resources_bbrc
