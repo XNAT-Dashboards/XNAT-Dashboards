caee658b8e921f82740c4e24211286fc1f47db87
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-07-19 22:22:22 +0530

    Using mock for testing timetaking function and resource testing

--- tests/test_save_to_pickle.py
+++ tests/test_save_to_pickle.py
@@ -2,7 +2,7 @@ from save_endpoint import save_to_pickle
 import pickle
 
 
-def test_save_data_and_user():
+def test_save_data_and_user(mocker):
 
     username = 'testUser'
     password = 'testPassword'
@@ -11,12 +11,38 @@ def test_save_data_and_user():
 
     saving_object = save_to_pickle.SaveToPk(
         username, password, server, ssl)
+
+    resource_return_value = {'date': '28', 'resources': 'test'}
+    data_return_value = 'Info Data'
+
+    mocker.patch(
+        'pyxnat_interface.data_fetcher.FetcherLong.get_resources',
+        return_value=resource_return_value)
+
+    mocker.patch(
+        'pyxnat_interface.data_fetcher.FetcherLong.get_experiment_resources',
+        return_value=resource_return_value)
+
+    mocker.patch(
+        'pyxnat_interface.data_fetcher.Fetcher.fetch_all',
+        return_value=data_return_value)
+
+    saving_object = save_to_pickle.SaveToPk(
+        username, password, server, ssl)
+
+    saving_object.save_resources()
+
+    with open('pickles/resources/testUser.pickle', 'rb') as handle:
+        res = pickle.load(handle)
+
+    assert type(res) == dict
+
     saving_object.save_data()
 
     with open('pickles/users_data/testUser.pickle', 'rb') as handle:
         user_data = pickle.load(handle)
 
-    assert type(user_data['info']) == dict
+    assert type(user_data) == dict
 
     # Save user
 
