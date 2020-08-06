3347209339ef427dcdb2567e0bdf032a32f274aa
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-08-06 20:21:53 +0530

    Include longitudnal data

--- app/dashboards/controllers.py
+++ app/dashboards/controllers.py
@@ -8,6 +8,7 @@ from app.dashboards import model
 # Define the blueprint: 'dashboards', set its url prefix: app.url/dashboards
 dashboards = Blueprint('dashboards', __name__, url_prefix='/dashboards')
 
+longitudnal_data = []
 graph_data_stats = []  # Contains graph Data loaded globally for get request
 project_lists = []  # Contains project list globally for get request
 username = ''  # For saving username globally
@@ -47,6 +48,8 @@ def stats_db():
         password = user_details['password']
         server = user_details['server']
         ssl = False if user_details.get('ssl') is None else True
+
+        global longitudnal_data
         global graph_data_stats
         global project_lists
 
@@ -70,12 +73,14 @@ def stats_db():
                     user_data = data['info']
                     resources = data['resources']
                     resources_bbrc = data['resources_bbrc']
+                    l_data = model.load_longitudnal_data(server)
 
                     if user_data is not None:
                         plotting_object = graph_generator_DB.\
                             GraphGenerator(
                                 username,
                                 user_data,
+                                l_data,
                                 role_exist,
                                 config['project_visible'],
                                 resources,
@@ -83,6 +88,9 @@ def stats_db():
 
                         graph_data_stats = plotting_object.\
                             graph_generator()
+
+                        longitudnal_data = plotting_object.\
+                            graph_generator_longitudnal()
 
                         project_lists = plotting_object.\
                             project_list_generator()
@@ -101,6 +109,7 @@ def stats_db():
             return redirect(url_for('auth.login_DB'))
 
         else:
+            print(longitudnal_data)
             project_list = project_lists[0]
             project_list_ow_co_me = project_lists[1]
             graph_data = graph_data_stats[0]
@@ -109,6 +118,7 @@ def stats_db():
                                    graph_data=graph_data,
                                    project_list=project_list,
                                    stats_data=stats_data,
+                                   longitudnal_data=longitudnal_data,
                                    project_list_ow_co_me=project_list_ow_co_me,
                                    username=username.capitalize(),
                                    server=server,
@@ -128,6 +138,7 @@ def stats_db():
                                    graph_data=graph_data,
                                    project_list=project_list,
                                    stats_data=stats_data,
+                                   longitudnal_data=longitudnal_data,
                                    project_list_ow_co_me=project_list_ow_co_me,
                                    username=username.capitalize(),
                                    server=server,
