83f29e6ad7f2ba3726adcfb9249df8e2f74beb04
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-08-18 19:08:36 +0530

    Change pickle directory

--- download_data.py
+++ download_data.py
@@ -5,7 +5,7 @@ import os
 
 # Add path to the pickle file
 path_creator.set_pickle_path(
-    os.path.abspath('xnat_dashboards/pickles/data/general.pickle'))
+    os.path.abspath('xnat_dashboards/config/general.pickle'))
 
 # if true is given as an argument, it will skip the downloading of resources
 # Default is false
