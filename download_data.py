0c2509bdad4d144e05e26078bb637cd6b63ebd37
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-08-19 10:40:59 +0530

    run script when running as main

--- download_data.py
+++ download_data.py
@@ -2,11 +2,11 @@ from xnat_dashboards.pyxnat_interface import pickle_saver
 from xnat_dashboards import path_creator
 import os
 
+if __name__ == "__main__":
+    # Add path to the pickle file
+    path_creator.set_pickle_path(
+        os.path.abspath('xnat_dashboards/config/general.pickle'))
 
-# Add path to the pickle file
-path_creator.set_pickle_path(
-    os.path.abspath('xnat_dashboards/config/general.pickle'))
-
-# if true is given as an argument, it will skip the downloading of resources
-# Default is false
-pickle_saver.PickleSaver('xnat_dashboards/config/central.cfg', True)
+    # if true is given as an argument, it will skip the downloading
+    # of resourceS Default is false
+    pickle_saver.PickleSaver('xnat_dashboards/config/central.cfg', True)
--- server.py
+++ server.py
@@ -2,11 +2,14 @@ from xnat_dashboards.app import app
 from xnat_dashboards import path_creator
 import os
 
-# Default path to configuration and pickle files
-path_creator.set_dashboard_config_path(
-    os.path.abspath('xnat_dashboards/config/dashboard_config.json'))
-path_creator.set_pickle_path(
-    os.path.abspath('xnat_dashboards/config/general.pickle'))
 
-# Change localhost url or port here
-app.run(debug=True)
+if __name__ == "__main__":
+
+    # Default path to configuration and pickle files
+    path_creator.set_dashboard_config_path(
+        os.path.abspath('xnat_dashboards/config/dashboard_config.json'))
+    path_creator.set_pickle_path(
+        os.path.abspath('xnat_dashboards/config/general.pickle'))
+
+    # Change localhost url or port here
+    app.run(debug=True)
