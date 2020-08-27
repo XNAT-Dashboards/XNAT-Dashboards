ffbf1a7750be377d29b510d6caef37af7304cee8
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2020-08-27 21:25:11 +0530

    help info change

--- bin/run_dashboards.py
+++ bin/run_dashboards.py
@@ -19,23 +19,28 @@ the pypi xnat_dashboards package.
 ap = argparse.ArgumentParser()
 ap.add_argument(
     "-p", "--pickle", type=str,
-    help="Path to saved pickle file")
+    help="Path to saved pickle file Example:"
+    " path/test.pickle")
+
 ap.add_argument(
     "-c", "--config", type=str,
-    help="Path to configuration file")
-args = vars(ap.parse_args())
+    help="Path to configuration file Example:"
+    " path/dashboard_config.json")
 
 ap.add_argument(
     "-port", "--port", type=int,
-    help="Port number", default=5000)
+    help="Port number Example:"
+    " 5000", default=5000)
 
 ap.add_argument(
     "-url", "--url", type=str,
-    help="URL for the server", default='localhost')
+    help="URL for the server Example:"
+    " 127.0.0.1", default='localhost')
 
 ap.add_argument(
     "-debug", "--debug", type=bool,
-    help="Activate debugger", default=True)
+    help="Activate debugger Example:"
+    " 1(True) or 0(False)", default=0)
 
 args = vars(ap.parse_args())
 
@@ -44,7 +49,7 @@ if __name__ == "__main__":
 
     if args['pickle'] is None or args['config'] is None:
         logging.error(
-            "   Please provide path, name to both pickle and "
+            "   Please provide path to both pickle and "
             "dashboard configuration file")
     else:
         # Path to configuration and pickle files
@@ -53,7 +58,17 @@ if __name__ == "__main__":
         # Change localhost url or port here
 
         try:
-            app.run(host=args['url'], port=args['port'], debug=args['debug'])
+            debug = bool(int(args['debug']))
+        except ValueError:
+            # If some error occurs in debug variable make
+            # debugging as false
+            debug = False
+
+        try:
+            app.run(
+                host=args['url'],
+                port=args['port'],
+                debug=debug)
         except socket.gaierror:
             logging.error("  Wrong server url provided to run the application")
         except PermissionError:
