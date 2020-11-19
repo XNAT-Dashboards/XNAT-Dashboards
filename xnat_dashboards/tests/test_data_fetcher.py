7dc656cb560c749c78e0c6edb9436c92e091eff8
xgrg <goperto@gmail.com>
2020-11-19 10:28:11 +0100

    Removed redundant tests (if it is a list it has a length)

--- xnat_dashboards/tests/test_data_fetcher.py
+++ xnat_dashboards/tests/test_data_fetcher.py
@@ -11,13 +11,6 @@ def test_get_instance_details():
 
     details = df.get_instance_details(x)
 
-    # type
-    assert isinstance(details['projects'], list)
-    assert isinstance(details['subjects'], list)
-    assert isinstance(details['experiments'], list)
-    assert isinstance(details['scans'], list)
-
-    # length
     assert len(details['projects']) != 0
     assert len(details['subjects']) != 0
     assert len(details['experiments']) != 0
@@ -28,10 +21,6 @@ def test_get_resources():
 
     resources, bbrc_resources = df.get_resources(x)
     print(bbrc_resources)
-    # type
-    assert isinstance(resources, list)
-    assert isinstance(bbrc_resources, list)
 
-    # length
     assert len(resources) != 0
     assert len(bbrc_resources) != 0
