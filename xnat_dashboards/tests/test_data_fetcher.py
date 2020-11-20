4e94d6a42f1cbe06f7ee76eb17401ed544b6baf4
marinagrprat <mgarcia@barcelonabeta.org>
2020-11-20 21:57:42 +0100

    add pickle_save part to test_data_fetcher

--- xnat_dashboards/tests/test_data_fetcher.py
+++ xnat_dashboards/tests/test_data_fetcher.py
@@ -1,10 +1,10 @@
 from xnat_dashboards import data_fetcher as df
+from xnat_dashboards import pickle
 import pyxnat
 import os.path as op
 import xnat_dashboards
 
 fp = op.join(op.dirname(xnat_dashboards.__file__), '..', '.xnat.cfg')
-print(fp)
 x = pyxnat.Interface(config=fp)
 
 
@@ -22,4 +22,19 @@ def test_data_fetcher():
     assert len(resources) != 0
     assert len(bbrc_resources) != 0
 
-    df.longitudinal_data(details, resources)
+    longitudinal_data = df.longitudinal_data(details, resources)
+
+    pickle.save(x, fp)
+    with open(x.PICKLE_PATH, 'rb') as handle:
+        data = pickle.load(handle)
+
+    assert isinstance(data, dict)
+    assert data['server'] == "https://dev-xnat.barcelonabeta.org"
+    assert data['verify'] == 1
+    assert data['info']['data']['projects'] == details['projects']
+    assert data['info']['data']['subjects'] == details['subjects']
+    assert data['info']['data']['experiments'] == details['experiments']
+    assert data['info']['data']['scans'] == details['scans']
+    assert data['resources'] == resources
+    assert data['extra_resources'] == bbrc_resources
+    assert data['longitudinal'] == longitudinal_data
