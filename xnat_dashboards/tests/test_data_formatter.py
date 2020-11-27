ccab5ed3c2de4aab064b74ef236ce04258eb576f
marinagrprat <mgarcia@barcelonabeta.org>
2020-11-27 12:18:21 +0100

    add test_experiments_details & assertions for length

--- xnat_dashboards/tests/test_data_formatter.py
+++ xnat_dashboards/tests/test_data_formatter.py
@@ -13,6 +13,8 @@ def test_get_projects_details():
 
     assert isinstance(project_details['Number of Projects'], int)
     assert isinstance(project_details['Projects Visibility'], dict)
+    assert len(project_details['Number of Projects'] != 0)
+    assert len(project_details['Projects Visibility'] != 0)
 
 
 def test_get_subjects_details():
@@ -28,3 +30,26 @@ def test_get_subjects_details():
     assert isinstance(subject_details['Gender'], dict)
     assert isinstance(subject_details['Handedness'], dict)
     assert isinstance(subject_details['Subjects/Project'], dict)
+
+    assert len(subject_details['Number of Subjects'] != 0)
+    assert len(subject_details['Age Range'] != 0)
+    assert len(subject_details['Gender'] != 0)
+    assert len(subject_details['Handedness'] != 0)
+    assert len(subject_details['Subjects/Project'] != 0)
+
+
+def test_get_experiments_details():
+
+    with open(pickle_path, 'rb') as handle:
+        data = pickle.load(handle)
+
+    experiments = data['info']['experiments']
+    experiments_details = df.Formatter().get_experiments_details(experiments)
+
+    assert isinstance(experiments_details['Number of Experiments'], int)
+    assert isinstance(experiments_details['Experiments/Project'], dict)
+    assert isinstance(experiments_details['Experiment Types'], dict)
+
+    assert len(experiments_details['Number of Experiments'] != 0)
+    assert len(experiments_details['Experiments/Project'] != 0)
+    assert len(experiments_details['Experiment Types'] != 0)
