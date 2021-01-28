e55264c552587af76356c3c8655cff88e6764451
marinagrprat <mgarcia@barcelonabeta.org>
2021-01-28 14:55:58 +0100

    Backup of the last valid pickle file

--- dashboards/pickle.py
+++ dashboards/pickle.py
@@ -16,32 +16,43 @@ def save(x, fp):
                         "different form the provided server URL"
                     raise Exception(msg)
 
-    details = data_fetcher.get_instance_details(x)
+    # Copy of the last valid pickle file
+    backup = p
 
-    resources, bbrc_resources = data_fetcher.get_resources(x)
+    try:
+        details = data_fetcher.get_instance_details(x)
 
-    res = ['FREESURFER6', 'FREESURFER6_HIRES', 'ASHS', 'BAMOS']
-    long_data = {}
-    for resource_name in res:
-        dt, n_res = data_fetcher.resource_monitor(x, resources, resource_name)
-        try:
-            long_data = p['longitudinal_data']
-            if resource_name not in long_data:
+        resources, bbrc_resources = data_fetcher.get_resources(x)
+
+        res = ['FREESURFER6', 'FREESURFER6_HIRES', 'ASHS', 'BAMOS']
+        long_data = {}
+
+        for resource_name in res:
+            dt, n_res = data_fetcher.resource_monitor(x, resources, resource_name)
+            try:
+                long_data = p['longitudinal_data']
+                if resource_name not in long_data:
+                    long_data[resource_name] = {dt: n_res}
+                long_data[resource_name][dt] = n_res
+            except:
                 long_data[resource_name] = {dt: n_res}
-            long_data[resource_name][dt] = n_res
-        except:
-            long_data[resource_name] = {dt: n_res}
 
-    d = {'server': x._server,
-         'verify': x._verify,
-         'info': details,
-         'resources': resources,
-         'extra_resources': bbrc_resources,
-         'longitudinal_data': long_data}
-    p.update(d)
+        d = {'server': x._server,
+             'verify': x._verify,
+             'info': details,
+             'resources': resources,
+             'extra_resources': bbrc_resources,
+             'longitudinal_data': long_data}
+        p.update(d)
 
-    # Save all the data to pickle
-    with open(fp, 'wb') as h:
-        pickle.dump(p, h, protocol=pickle.HIGHEST_PROTOCOL)
+        # Save all the data to pickle
+        with open(fp, 'wb') as h:
+            pickle.dump(p, h, protocol=pickle.HIGHEST_PROTOCOL)
 
-    print("Pickle file successfully saved at", fp)
+        print("Pickle file successfully saved at", fp)
+    except:
+        # Save backup file
+        with open(fp, 'wb') as h:
+            pickle.dump(backup, h, protocol=pickle.HIGHEST_PROTOCOL)
+
+        print("Backup file successfully saved at", fp)
