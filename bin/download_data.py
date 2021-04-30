8d4fa849a210f4037e40dc544fda12414b370c88
jhuguetn <jhuguetn@gmail.com>
2021-04-30 19:05:48 +0200

    REF: download_data.py (logging, close jsession)

--- bin/download_data.py
+++ bin/download_data.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
+
 import os.path as op
 import pyxnat
+import logging as log
+# import sys
+# sys.path.insert(0, op.abspath('..'))
 
 
 def create_args():
-
     import argparse
+
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--config", type=argparse.FileType('r'),
+    ap.add_argument("-c", "--config", type=argparse.FileType('r'),
                     help="Path to pyxnat configuration file",
                     required=True)
-    ap.add_argument("-o", "--pickle", type=str,
+    ap.add_argument("-p", "--pickle", type=str,
                     help="Path where the pickle file will be created",
                     required=True)
     return ap
@@ -20,9 +24,13 @@ if __name__ == "__main__":
     parser = create_args()
     args = parser.parse_args()
 
+    log.basicConfig(level=log.INFO, datefmt='%Y-%m-%d %H:%M:%S',
+                    format='%(asctime)s:%(module)s:%(levelname)s:%(message)s')
+
     config = args.config.name
     fp = op.abspath(args.pickle)
 
     from dashboards import pickle
     x = pyxnat.Interface(config=config)
     pickle.save(x, fp)
+    x.close_jsession()
