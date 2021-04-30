126d2ca59f787c49a788adf3515f0e3483e074ce
jhuguetn <jhuguetn@gmail.com>
2021-04-30 19:02:09 +0200

    REF: run_dashboards.py

--- bin/run_dashboards.py
+++ bin/run_dashboards.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
 
-from dashboards.app import app
-from dashboards import config
-import os
-import argparse
-from argparse import RawTextHelpFormatter
+import os.path as op
+# import sys
+# sys.path.insert(0, op.abspath('..'))
 
-desc = 'Run the webapp given a previously generated pickle file (yielded by'\
-       'download_data.py) and a configuration file (JSON file describing the '\
-       'different plots and access rights)'
 
-ap = argparse.ArgumentParser(description=desc,
-                             formatter_class=RawTextHelpFormatter)
-ap.add_argument('-p', '--pickle', help='Path to pickle', required=True)
-ap.add_argument('-c', '--config', help='Path to configuration file',
-                required=True)
-ap.add_argument('-P', '--port', help='Port number', default=5000)
-ap.add_argument('-u', '--url', help='URL for the server', default='0.0.0.0')
-ap.add_argument('--debug', help='Debug mode', action='store_true',
-                default=False)
-args = ap.parse_args()
+def create_args():
+    import argparse
+    from argparse import RawTextHelpFormatter
+
+    desc = 'Run the webapp given a previously generated pickle file (yielded '\
+           'by download_data.py) and a configuration file (JSON file describing '\
+           'the different plots and access rights)'
+
+    ap = argparse.ArgumentParser(description=desc,
+                                 formatter_class=RawTextHelpFormatter)
+    ap.add_argument('-p', '--pickle', help='Path to pickle', required=True)
+    ap.add_argument('-c', '--config', help='Path to configuration file',
+                    required=True)
+    ap.add_argument('-P', '--port', help='Port number', default=5000)
+    ap.add_argument('-u', '--url', help='URL for the server', default='0.0.0.0')
+    ap.add_argument('--debug', help='Debug mode', action='store_true',
+                    default=False)
+    return ap
 
 
 if __name__ == "__main__":
-    config.DASHBOARD_CONFIG_PATH = os.path.abspath(args.config)
-    config.PICKLE_PATH = os.path.abspath(args.pickle)
+    parser = create_args()
+    args = parser.parse_args()
 
+    from dashboards import config
+    from dashboards.app import app
+
+    config.DASHBOARD_CONFIG_PATH = op.abspath(args.config)
+    config.PICKLE_PATH = op.abspath(args.pickle)
     app.run(host=args.url, port=args.port, debug=args.debug)
